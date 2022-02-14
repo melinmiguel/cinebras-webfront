c10cd2f7d432acbf19bff94714e081f59ccbcde8
melinmiguel <melinmiguel@gmail.com>
2022-02-14 17:14:03 -0300

    carrossel

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,6 +1,17 @@
+/* eslint-disable import/extensions */
+/* eslint-disable jsx-a11y/alt-text */
 import React from 'react';
+import { LogoContainer } from '../../components/TemplatePage/styles';
 // import Logo from 'src/assets/images/logo.png';
 import LabelBuscar from './Components/LabelBuscar';
+import Logo from '../../img/logo.png';
+import LogoHavan from '../../img/logoHavan.png';
+import LogoItau from '../../img/logoItau.png';
+import Carousel from '../../components/Carousel'; // carrossel!
+import dadosIniciais from '../../data/dados_iniciais.json'; // carrossel!
+import theme from '../../styles/theme';
+import backgroundImageHeader from '../../img/background_header4.png';
+
 import {
   Image,
   Input,
@@ -16,32 +27,29 @@ import {
   ContainerFooterEmail,
   ContainerFilmesCarroussel,
   AnuncianteVertical,
+  LogoAnunciante,
+  AnuncianteMiniVertical,
 } from './styles';
 
 const Home: React.FC = () => {
   return (
-    <>
+    <div style={{ backgroundColor: `${theme.verdeCinebras}` }}>
       <Header>
         <Image>
-          *LOGO CONTAINER
-          {/* <img src={Logo} /> */}
+          <img src={Logo} style={{ width: '272px' }} />
         </Image>
-        <Input className="pesquisa" placeholder="o que deseja encontrar?" />
+        <Input className="pesquisa" placeholder="O que você procura?" />
       </Header>
-      <AnuncianteHorizontal>Anunciante Horizontal</AnuncianteHorizontal>
+      <AnuncianteHorizontal>
+        <LogoAnunciante>
+          <img src={LogoHavan} style={{ height: '161px', width: '277px' }} />
+        </LogoAnunciante>
+        <CarrouselCapas style={{ marginTop: '25px' }} />
+      </AnuncianteHorizontal>
       <Body>
         <ContainerFilmesCarroussel>
-          <FilmesCarrousel>
-            <div>
-              <p>Genero dos filmes a serem mostrados 1</p>
-              <p>#chave1 #chave2 #chave3</p>
-            </div>
-            <CarrouselCapas>
-              <CapaFilme />
-              <CapaFilme />
-              <CapaFilme />
-            </CarrouselCapas>
-          </FilmesCarrousel>
+          <Carousel category={dadosIniciais.categorias[1]} ignoreFirstVideo />
+          {/* CAROSSEL!!! */}
           <FilmesCarrousel>
             <div>
               <p>Genero dos filmes a serem mostrados 2</p>
@@ -53,6 +61,20 @@ const Home: React.FC = () => {
               <CapaFilme />
             </CarrouselCapas>
           </FilmesCarrousel>
+          <AnuncianteMiniVertical>
+            <img
+              src={LogoItau}
+              style={{
+                height: '223px',
+                width: '277px',
+                marginRight: '30px',
+              }}
+            />
+            <CapaFilme />
+            <CapaFilme />
+            <CapaFilme />
+          </AnuncianteMiniVertical>
+
           <FilmesCarrousel>
             <div>
               <p>Genero dos filmes a serem mostrados 3</p>
@@ -98,7 +120,8 @@ const Home: React.FC = () => {
         </ContainerFooterEmail>
       </Footer>
       <Body />
-    </>
+    </div>
   );
 };
+
 export default Home;
--- src/components/NavBar/styles.ts
+++ src/components/NavBar/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import theme from '../../styles/theme';
 
 export const Li = styled.li`
   margin-right: 1em;
@@ -9,9 +10,12 @@ export const Ul = styled.ul`
   display: flex;
   list-style: none;
   color: white;
+  margin-top: 26px;
 `;
 
 export const Container = styled.div`
   font-size: 24px;
   margin-left: 30px;
+  z-index: 10;
+  background-color: ${theme.verdeCinebras};
 `;
